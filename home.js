2b0ab740b4de8a7b84fb730969ca6842ef597c0e
chapsticking <natepahlow00@gmail.com>
2025-06-07 16:57:35 -0500

    updated readme, added countdown timer, etc.

--- home.js
+++ home.js
@@ -689,3 +689,66 @@ function downloadCSV(csvString, filename = 'chart_data.csv') {
 
   URL.revokeObjectURL(url);
 }
+
+//-----Nates Silly Countdown to Retirement-----
+
+function calculateDays(startDate, endDate) {
+  let start = new Date(startDate);
+  let end = new Date(endDate);
+  let timeDifference = end - start;
+  let daysDifference = timeDifference / (1000 * 3600 * 24);
+  return daysDifference;
+}
+
+const countDownClock = (number = 100, format = 'seconds') => {
+  const d = document;
+  const daysElement = d.querySelector('.days');
+  const hoursElement = d.querySelector('.hours');
+  const minutesElement = d.querySelector('.minutes');
+  const secondsElement = d.querySelector('.seconds');
+  let countdown;
+  convertFormat(format);
+
+  function convertFormat(format) {
+    switch (format) {
+      case 'seconds':
+        return timer(number);
+      case 'minutes':
+        return timer(number * 60);
+      case 'hours':
+        return timer(number * 60 * 60);
+      case 'days':
+        return timer(number * 60 * 60 * 24);
+    }
+  }
+
+  function timer(seconds) {
+    const now = Date.now();
+    const then = now + seconds * 1000;
+
+    countdown = setInterval(() => {
+      const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+      if (secondsLeft <= 0) {
+        clearInterval(countdown);
+        return;
+      }
+
+      displayTimeLeft(secondsLeft);
+    }, 1000);
+  }
+
+  function displayTimeLeft(seconds) {
+    daysElement.textContent = Math.floor(seconds / 86400);
+    hoursElement.textContent = Math.floor((seconds % 86400) / 3600);
+    minutesElement.textContent = Math.floor(((seconds % 86400) % 3600) / 60);
+    secondsElement.textContent = seconds % 60 < 10 ? `0${seconds % 60}` : seconds % 60;
+  }
+};
+
+/*
+  start countdown
+  enter number and format
+  days, hours, minutes or seconds
+*/
+countDownClock(calculateDays(Date.now(), '2031-11-02'), 'days');
